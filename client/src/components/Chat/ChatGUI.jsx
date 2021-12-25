3d8ad3e82ac64f5bba3d8215352675cae65df3c2
hannanxo <pipebombbest434@gmail.com>
2021-12-26 03:50:06 +0500

    Added scroll functionality

--- client/src/components/Chat/ChatGUI.jsx
+++ client/src/components/Chat/ChatGUI.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import "./ChatGUI.css";
 import React, { Fragment } from "react";
+import { useEffect, useRef } from "react";
 export default function ChatGUI() {
   const {
     register,
@@ -11,6 +12,17 @@ export default function ChatGUI() {
   const onSubmit = data => {
     console.log(data);
   };
+
+  // useEffect(() => {}, []);
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView();
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
 
   return (
     <Fragment>
@@ -25,6 +37,7 @@ export default function ChatGUI() {
             </span>
           </div>
         </header>
+
         <main className="msger-chat">
           <div className="msg left-msg">
             <div className="msg-img" />
@@ -46,7 +59,49 @@ export default function ChatGUI() {
               <div className="msg-text">You can change your name in JS section!</div>
             </div>
           </div>
+          <div className="msg left-msg">
+            <div className="msg-img" />
+            <div className="msg-bubble">
+              <div className="msg-info">
+                <div className="msg-info-name">BOT</div>
+                <div className="msg-info-time">12:45</div>
+              </div>
+              <div className="msg-text">Hi, welcome to SimpleChat! Go ahead and send me a message. 😄</div>
+            </div>
+          </div>
+          <div className="msg right-msg">
+            <div className="msg-img" />
+            <div className="msg-bubble">
+              <div className="msg-info">
+                <div className="msg-info-name">Sajad</div>
+                <div className="msg-info-time">12:46</div>
+              </div>
+              <div className="msg-text">You can change your name in JS section!</div>
+            </div>
+          </div>{" "}
+          <div className="msg left-msg">
+            <div className="msg-img" />
+            <div className="msg-bubble">
+              <div className="msg-info">
+                <div className="msg-info-name">BOT</div>
+                <div className="msg-info-time">12:45</div>
+              </div>
+              <div className="msg-text">Hi, welcome to SimpleChat! Go ahead and send me a message. 😄</div>
+            </div>
+          </div>
+          <div className="msg right-msg">
+            <div className="msg-img" />
+            <div className="msg-bubble">
+              <div className="msg-info">
+                <div className="msg-info-name">Sajad</div>
+                <div className="msg-info-time">12:46</div>
+              </div>
+              <div className="msg-text">You can change your name in JS section!</div>
+            </div>
+          </div>{" "}
+          <div ref={messagesEndRef} />
         </main>
+
         <form className="msger-inputarea">
           <input type="text" className="msger-input" placeholder="Enter your message..." />
           <button type="submit" className="msger-send-btn">
