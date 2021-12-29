adda3c421290e36b3d72c3e140ea7acdbdd6d662
hassamboi <hassamuddin.all@gmail.com>
2021-12-30 01:11:51 +0500

    Fixed messages render bug

--- client/src/components/Chat/ChatGUI.jsx
+++ client/src/components/Chat/ChatGUI.jsx
@@ -1,23 +1,18 @@
 import "./ChatGUI.css";
 import React, { Fragment, useState } from "react";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { Para } from "../Forms/FormElements";
 import { io } from "socket.io-client";
 import { useAuth } from "../../hooks/useAuth";
 import api from "../../api/posts";
 import MessageList from "../MessageList";
+
+// global variable for socket so the connection is established once
 let socket;
+
 export default function ChatGUI() {
   const [messages, setMessages] = useState([]);
-  const [render, setRender] = useState("");
+  const [inputMessage, setInputMessage] = useState("");
   const { token } = useAuth();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm();
 
   const getMessages = async () => {
     const response = await api.get("/chat", {
@@ -29,52 +24,40 @@ export default function ChatGUI() {
   };
 
   useEffect(() => {
-    const controller = new AbortController();
     socket = io("http://localhost:5000");
     getMessages();
-    socket.on("response", response => {
-      setRender(...render, "screw");
-      getMessages();
-    });
-
-    return () => {
-      controller.abort();
-    };
   }, []);
 
-  const onSubmit = async data => {
-    setRender("");
-    const userdata = { ...data, token };
+  const handleSubmit = e => {
+    e.preventDefault();
+    const text = inputMessage;
+    const userdata = { text, token };
     socket.emit("message", userdata);
-    reset({ text: "" });
+    setInputMessage("");
+    socket.on("response", response => {
+      setMessages(() => {
+        return [...messages, { text, response }];
+      });
+    });
   };
 
   return (
     <Fragment>
       <section className="msger">
-        <header className="msger-header">
-          {" "}
-          <Para>{errors.text?.message}</Para>
-        </header>
+        <header className="msger-header"></header>
 
-        <main className="msger-chat">{messages && <MessageList messages={messages} />}</main>
+        <main className="msger-chat">
+          {messages && <MessageList messages={messages} />}
+        </main>
 
-        <form className="msger-inputarea" onSubmit={handleSubmit(onSubmit)}>
+        <form className="msger-inputarea" onSubmit={e => handleSubmit(e)}>
           <input
             type="text"
             name="text"
             className="msger-input"
             placeholder="Enter your message..."
-            {...register("text", {
-              minLength: {
-                value: 2,
-                message: "Message is too short",
-              },
-              maxLength: {
-                value: 100,
-                message: "Message is too long",
-              },
-            })}
+            onChange={e => setInputMessage(e.target.value)}
+            value={inputMessage}
           />
 
           <button type="submit" className="msger-send-btn">
--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -2,18 +2,21 @@ import { useAuth } from "../hooks/useAuth";
 
 export default function MessageList({ messages }) {
   const { user } = useAuth();
+
   return (
     <>
       {messages
         .slice(0)
         .reverse()
-        .map(message => (
-          <div key={message._id}>
+        .map((message, index) => (
+          <div key={message._id ? message._id : index}>
             <div className="msg right-msg">
               <div className="msg-img" />
               <div className="msg-bubble">
                 <div className="msg-info">
-                  <div className="msg-info-name">{user ? user.name : "You"}</div>
+                  <div className="msg-info-name">
+                    {user ? user.name : "You"}
+                  </div>
                 </div>
                 <div className="msg-text">{message.text}</div>
               </div>
